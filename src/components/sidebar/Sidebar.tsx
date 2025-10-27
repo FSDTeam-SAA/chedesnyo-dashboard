644f898b27d3b63911f1a1a034e5b023508feefe
jarifMohammed <mohammedjarif@protonmail.com>
2025-10-27 15:59:49 +0600

    new all

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,13 +3,10 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import {
-  LayoutDashboard,
-  LogOut,
-  Grip,
-  ShoppingBasket,
-} from "lucide-react";
+import { LayoutDashboard, LogOut, Grip, ShoppingBasket } from "lucide-react";
 import Image from "next/image";
+import { useState } from "react";
+import LogoutModal from "../Dialog/LogOutModal";
 
 const navigation = [
   { name: "Dashboard", href: "/", icon: LayoutDashboard },
@@ -21,11 +18,11 @@ const navigation = [
   { name: "Industry List", href: "/industry-list", icon: ShoppingBasket },
   { name: "Blog Management", href: "/blog-management", icon: ShoppingBasket },
   { name: "Settings", href: "/settings", icon: ShoppingBasket },
-
 ];
 
 export function Sidebar() {
   const pathname = usePathname();
+  const [isLogoutOpen, setIsLogoutOpen] = useState(false);
 
   return (
     <div className="flex h-screen sticky top-0 w-[420px] flex-col z-50 shadow-[0px_16px_48px_0px_#00000029] bg-gray-50">
@@ -80,14 +77,20 @@ export function Sidebar() {
 
       {/* Logout fixed at bottom */}
       <div className="p-3">
-        <div className="flex items-center gap-3 px-4 py-2.5 rounded-xl cursor-pointer 
-                  bg-gradient-to-r from-red-500/10 to-transparent 
-                  text-red-600 font-semibold text-base transition-all duration-300 
-                  hover:from-red-600 hover:to-red-700 hover:text-white hover:shadow-md">
+        <div
+          className="flex items-center gap-3 px-4 py-2.5 rounded-xl cursor-pointer 
+                     bg-gradient-to-r from-red-500/10 to-transparent 
+                     text-red-600 font-semibold text-base transition-all duration-300 
+                     hover:from-red-600 hover:to-red-700 hover:text-white hover:shadow-md"
+          onClick={() => setIsLogoutOpen(true)}
+        >
           <LogOut className="h-5 w-5 transition-transform duration-300 group-hover:rotate-12" />
           <span>Log Out</span>
         </div>
       </div>
+
+      {/* Logout modal */}
+      <LogoutModal isOpen={isLogoutOpen} setIsOpen={setIsLogoutOpen} />
     </div>
   );
 }
